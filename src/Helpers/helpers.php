a644d4e19731540e03ba0384e2ef67318150caad
Sanjana Rathnayake <138987781+sanjanarathnyke@users.noreply.github.com>
2025-11-23 10:27:54 +0530

    Add files via upload

---
+++ src/Helpers/helpers.php
@@ -0,0 +1,8 @@
+<?php
+
+if (! function_exists('say_hello')) {
+    function say_hello($name)
+    {
+        return "Hello, $name from Vertexcore Helpers Package!";
+    }
+}
---
+++ src/HelpersServiceProvider.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace Vertexcore\Helpers;
+
+use Illuminate\Support\ServiceProvider;
+
+class HelpersServiceProvider extends ServiceProvider
+{
+    public function register()
+    {
+        // Auto-loaded via composer.json (files autoload)
+    }
+
+    public function boot()
+    {
+        //
+    }
+}
